883d33e4b200f86b3dbe64681edb11cd94392336
Artem Ilin <timsanru39@gmail.com>
2017-11-26 15:32:41 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/cemeraappkt/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.cemeraappkt
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST_CODE = 0
+    lateinit var nameTxt: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        photoBtnView.setOnClickListener(View.OnClickListener {
+            nameTxt = nameEditTxtView.text.toString()
+            if(nameTxt.isNotEmpty()){
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if(cameraIntent.resolveActivity(packageManager) != null){
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+                }
+             }else{
+                Toast.makeText(applicationContext, R.string.type_name_toast, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                 if(resultCode == Activity.RESULT_OK && data != null){
+                     val showPhotoIntent = Intent(applicationContext, PhotoActivity::class.java)
+                     val bundle = Bundle()
+                     bundle.putString("name", nameTxt)
+                     bundle.putParcelable("photo", data.extras.get("data") as Bitmap)
+                     showPhotoIntent.putExtras(bundle)
+                     startActivity(showPhotoIntent)
+                 }
+            }
+            else -> {
+                Toast.makeText(applicationContext, R.string.unrecognized_toast, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
